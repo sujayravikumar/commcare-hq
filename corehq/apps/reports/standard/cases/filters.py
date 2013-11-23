bb4e3770375a9a17bb7d375fcd15b764d5805392
dmyung <dmyung@dimagi.com>
2013-11-22 22:02:58 -0500

    naive csv text box for testing arbitrary case props
    in advanced case search

--- corehq/apps/reports/standard/cases/filters.py
+++ corehq/apps/reports/standard/cases/filters.py
@@ -1,6 +1,12 @@
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_noop
+from corehq.apps.reports.filters.base import BaseReportFilter
 from corehq.apps.reports.filters.search import SearchFilter
 
 class CaseSearchFilter(SearchFilter):
     search_help_inline = mark_safe(ugettext_noop("""Search any text, or use a targeted query. For more info see the <a href='https://wiki.commcarehq.org/display/commcarepublic/Advanced+Case+Search' target='_blank'>Case Search</a> help page"""))
+
+
+class CasePropertiesFilter(SearchFilter):
+    slug = "property_query"
+    label = ugettext_noop("Case Properties")
