3203a54efcd0da913092d3390d42c497dcfe79c4
Nick Pellegrino <npellegrino@dimagi.com>
2014-10-08 17:25:01 -0400

    add calculated properties

--- corehq/apps/api/domain_metadata.py
+++ corehq/apps/api/domain_metadata.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, Http404
 from django.views.generic import View
 from corehq import Domain
+from corehq.apps.es.domains import DomainES
 
 from dimagi.utils.web import json_response
 
@@ -14,7 +15,18 @@ class DomainMetadataAPI(View):
 
     def get_metadata(self, domain):
         return {
+            "calculated_properties": self.get_calculated_properties(domain),
             "domain_properties": self.get_domain_properties(domain),
+        }
+
+    def get_calculated_properties(self, domain):
+        es_data = (DomainES()
+                   .in_domains([domain.name])
+                   .run()
+                   .raw_hits[0]['_source'])
+        return {
+            raw_hit: es_data[raw_hit]
+            for raw_hit in es_data if raw_hit[:3] == 'cp_'
         }
 
     def get_domain_properties(self, domain):
