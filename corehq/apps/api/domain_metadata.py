e99b7aee58e0b797511c28580305604a3582aa6f
Nick Pellegrino <npellegrino@dimagi.com>
2014-10-08 16:51:29 -0400

    add DomainMetadataAPI class

---
+++ corehq/apps/api/domain_metadata.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse, Http404
+from django.views.generic import View
+from corehq import Domain
+
+from dimagi.utils.web import json_response
+
+
+class DomainMetadataAPI(View):
+    def get(self, *args, **kwargs):
+        domain = Domain.get_by_name(kwargs.get('domain'))
+        if not domain:
+            raise Http404
+        return json_response(self.get_metadata(domain))
+
+    def get_metadata(self, domain):
+        return {
+        }
