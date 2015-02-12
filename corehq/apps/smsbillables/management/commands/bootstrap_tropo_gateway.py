c724344c79b6a4c4aa4831bd6903733cbb447122
Nick Pellegrino <npellegrino@dimagi.com>
2015-02-12 17:10:49 -0500

    bootstrap tropo

--- corehq/apps/smsbillables/management/commands/bootstrap_tropo_gateway.py
+++ corehq/apps/smsbillables/management/commands/bootstrap_tropo_gateway.py
@@ -3,10 +3,35 @@ from django.core.management.base import LabelCommand
 
 from corehq.apps.tropo.api import TropoBackend
 from corehq.apps.sms.models import INCOMING, OUTGOING
-from corehq.apps.smsbillables.models import SmsGatewayFee
+from corehq.apps.smsbillables.models import SmsGatewayFee, SmsGatewayFeeCriteria
 
 logger = logging.getLogger('accounting')
 
+
+def bootstrap_tropo_gateway(orm):
+    sms_gateway_fee_class = orm['smsbillables.SmsGatewayFee'] if orm else SmsGatewayFee
+    sms_gateway_fee_criteria_class = orm['smsbillables.SmsGatewayFeeCriteria'] if orm else SmsGatewayFeeCriteria
+
+    SmsGatewayFee.create_new(TropoBackend.get_api_id(), INCOMING, 0.01,
+                             fee_class=sms_gateway_fee_class, criteria_class=sms_gateway_fee_criteria_class)
+
+    rates_csv = open('corehq/apps/smsbillables/management/'
+                     'pricing_data/tropo_international_rates_2013-12-19.csv', 'r')
+    for line in rates_csv.readlines():
+        data = line.split(',')
+        if data[1] == 'Fixed Line' and data[4] != '\n':
+            SmsGatewayFee.create_new(TropoBackend.get_api_id(),
+                                     OUTGOING,
+                                     float(data[4].rstrip()),
+                                     country_code=int(data[2]),
+                                     fee_class=sms_gateway_fee_class, criteria_class=sms_gateway_fee_criteria_class)
+    rates_csv.close()
+
+    # Fee for invalid phonenumber
+    SmsGatewayFee.create_new(TropoBackend.get_api_id(), OUTGOING, 0.01, country_code=None,
+                             fee_class=sms_gateway_fee_class, criteria_class=sms_gateway_fee_criteria_class)
+
+    logger.info("Updated Tropo gateway fees.")
 
 class Command(LabelCommand):
     help = "bootstrap Tropo gateway fees"
@@ -14,20 +39,4 @@ class Command(LabelCommand):
     label = ""
 
     def handle(self, *args, **options):
-        SmsGatewayFee.create_new(TropoBackend.get_api_id(), INCOMING, 0.01)
-
-        rates_csv = open('corehq/apps/smsbillables/management/'
-                         'pricing_data/tropo_international_rates_2013-12-19.csv', 'r')
-        for line in rates_csv.readlines():
-            data = line.split(',')
-            if data[1] == 'Fixed Line' and data[4] != '\n':
-                SmsGatewayFee.create_new(TropoBackend.get_api_id(),
-                                         OUTGOING,
-                                         float(data[4].rstrip()),
-                                         country_code=int(data[2]))
-        rates_csv.close()
-
-        # Fee for invalid phonenumber
-        SmsGatewayFee.create_new(TropoBackend.get_api_id(), OUTGOING, 0.01, country_code=None)
-
-        logger.info("Updated Tropo gateway fees.")
+        bootstrap_tropo_gateway(None)
--- settings.py
+++ settings.py
@@ -1112,7 +1112,7 @@ SMS_HANDLERS = [
 SMS_LOADED_BACKENDS = [
     # "corehq.apps.unicel.api.UnicelBackend",
     "corehq.apps.mach.api.MachBackend",
-    # "corehq.apps.tropo.api.TropoBackend",
+    "corehq.apps.tropo.api.TropoBackend",
     # "corehq.apps.sms.backend.http_api.HttpBackend",
     # "corehq.apps.telerivet.models.TelerivetBackend",
     # "corehq.apps.sms.test_backend.TestSMSBackend",
