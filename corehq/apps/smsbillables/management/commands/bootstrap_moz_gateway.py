a34297ee0dc31ed7b9fde50d182fb50b23acc1e3
Nick Pellegrino <npellegrino@dimagi.com>
2015-02-13 11:33:02 -0500

    moz bootstrap styling

--- corehq/apps/smsbillables/management/commands/bootstrap_moz_gateway.py
+++ corehq/apps/smsbillables/management/commands/bootstrap_moz_gateway.py
@@ -12,28 +12,102 @@ from corehq.apps.smsbillables.models import SmsGatewayFee
 logger = logging.getLogger('accounting')
 
 
+def bootstrap_moz_gateway(orm):
+    mzn, _ = (orm['accounting.Currency'] if orm else Currency).objects.get_or_create(code='MZN')
+
+    SmsGatewayFee.create_new(
+        'SISLOG',
+        INCOMING,
+        Decimal('1.4625'),
+        country_code='258',
+        prefix='82',
+        currency=mzn,
+    )
+    SmsGatewayFee.create_new(
+        'SISLOG',
+        INCOMING,
+        Decimal('1.4625'),
+        country_code='258',
+        prefix='83',
+        currency=mzn,
+    )
+    SmsGatewayFee.create_new(
+        'SISLOG',
+        INCOMING,
+        Decimal('1.4625'),
+        country_code='258',
+        prefix='84',
+        currency=mzn,
+    )
+    SmsGatewayFee.create_new(
+        'SISLOG',
+        INCOMING,
+        Decimal('0.702'),
+        country_code='258',
+        prefix='86',
+        currency=mzn,
+    )
+    SmsGatewayFee.create_new(
+        'SISLOG',
+        INCOMING,
+        Decimal('0.702'),
+        country_code='258',
+        prefix='87',
+        currency=mzn,
+    )
+
+    backend = HttpBackend.get('7ddf3301c093b793c6020ebf755adb6f')
+    SmsGatewayFee.create_new(
+        backend.get_api_id(),
+        OUTGOING,
+        Decimal('0.3627'),
+        backend_instance=backend._id,
+        country_code='258',
+        prefix='82',
+        currency=mzn,
+    )
+    SmsGatewayFee.create_new(
+        backend.get_api_id(),
+        OUTGOING,
+        Decimal('0.3627'),
+        backend_instance=backend._id,
+        country_code='258',
+        prefix='83',
+        currency=mzn,
+    )
+    SmsGatewayFee.create_new(
+        backend.get_api_id(),
+        OUTGOING,
+        Decimal('0.3627'),
+        backend_instance=backend._id,
+        country_code='258',
+        prefix='84',
+        currency=mzn,
+    )
+    SmsGatewayFee.create_new(
+        backend.get_api_id(),
+        OUTGOING,
+        Decimal('1.1232'),
+        backend_instance=backend._id,
+        country_code='258',
+        prefix='86',
+        currency=mzn,
+    )
+    SmsGatewayFee.create_new(
+        backend.get_api_id(),
+        OUTGOING,
+        Decimal('1.1232'),
+        backend_instance=backend._id,
+        country_code='258',
+        prefix='87',
+        currency=mzn,
+    )
+
+
 class Command(LabelCommand):
     help = "bootstrap MOZ global SMS backend gateway fees"
     args = ""
     label = ""
 
     def handle(self, *args, **options):
-        mzn, _ = Currency.objects.get_or_create(code='MZN')
-
-        SmsGatewayFee.create_new('SISLOG', INCOMING, Decimal('1.4625'), country_code='258', prefix='82', currency=mzn)
-        SmsGatewayFee.create_new('SISLOG', INCOMING, Decimal('1.4625'), country_code='258', prefix='83', currency=mzn)
-        SmsGatewayFee.create_new('SISLOG', INCOMING, Decimal('1.4625'), country_code='258', prefix='84', currency=mzn)
-        SmsGatewayFee.create_new('SISLOG', INCOMING, Decimal('0.702'), country_code='258', prefix='86', currency=mzn)
-        SmsGatewayFee.create_new('SISLOG', INCOMING, Decimal('0.702'), country_code='258', prefix='87', currency=mzn)
-
-        backend = HttpBackend.get('7ddf3301c093b793c6020ebf755adb6f')
-        SmsGatewayFee.create_new(backend.get_api_id(), OUTGOING, Decimal('0.3627'),
-            backend_instance=backend._id, country_code='258', prefix='82', currency=mzn)
-        SmsGatewayFee.create_new(backend.get_api_id(), OUTGOING, Decimal('0.3627'),
-            backend_instance=backend._id, country_code='258', prefix='83', currency=mzn)
-        SmsGatewayFee.create_new(backend.get_api_id(), OUTGOING, Decimal('0.3627'),
-            backend_instance=backend._id, country_code='258', prefix='84', currency=mzn)
-        SmsGatewayFee.create_new(backend.get_api_id(), OUTGOING, Decimal('1.1232'),
-            backend_instance=backend._id, country_code='258', prefix='86', currency=mzn)
-        SmsGatewayFee.create_new(backend.get_api_id(), OUTGOING, Decimal('1.1232'),
-            backend_instance=backend._id, country_code='258', prefix='87', currency=mzn)
+        bootstrap_moz_gateway(None)
