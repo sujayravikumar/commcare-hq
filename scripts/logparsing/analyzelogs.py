64038c443dca96ddfaabc0e645baad36cff0b836
Tyler Sheffels <tsheffels@dimagi.com>
2015-06-24 17:18:17 -0400

    Add decmial precision

--- scripts/logparsing/analyzelogs.py
+++ scripts/logparsing/analyzelogs.py
@@ -1,6 +1,7 @@
 import sys
 import json
 from collections import defaultdict
+from decimal import *
 
 
 if __name__ == '__main__':
@@ -11,6 +12,7 @@ if __name__ == '__main__':
     # Items in counts are (view, total time, total views)
     counts = sorted([(key, sum(value), len(value)) for key, value in hits_by_view.items()],
                     key=lambda (key, value, _): -value)
+    getcontext().prec = 7
     print '%6s %10s %10s %s' % ('Count', 'Time', 'Avg', 'View')
     for view, total_time, count in counts:
-        print '%6s %10s %10s %s' % (count, total_time, total_time/float(count), view)
+        print '%6s %10s %10s %s' % (count, total_time, Decimal(total_time/float(count)), view)
