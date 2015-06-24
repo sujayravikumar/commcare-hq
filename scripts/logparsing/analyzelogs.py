8c5a51e0b2ec892233632a1820c6fd8c6f514665
Tyler Sheffels <tsheffels@dimagi.com>
2015-06-24 16:58:38 -0400

    Remove print statement

--- scripts/logparsing/analyzelogs.py
+++ scripts/logparsing/analyzelogs.py
@@ -8,7 +8,6 @@ if __name__ == '__main__':
     for line in sys.stdin:
         url_info = json.loads(line)
         hits_by_view[url_info.get('view')].append(float(url_info['time']))
-    print hits_by_view
     counts = sorted([(key, sum(value)) for key, value in hits_by_view.items()],
                     key=lambda (key, value): -value)
     for view, count in counts:
