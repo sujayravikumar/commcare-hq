5fcd02698d0de870ca74c2fae4e35176fe8eeee6
Tyler Sheffels <tsheffels@dimagi.com>
2015-06-24 17:12:41 -0400

    Formatting

--- scripts/logparsing/analyzelogs.py
+++ scripts/logparsing/analyzelogs.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
     # Items in counts are (view, total time, total views)
     counts = sorted([(key, sum(value), len(value)) for key, value in hits_by_view.items()],
                     key=lambda (key, value, _): -value)
-    print '%6s %10s %s' % ('Count', 'Time', 'Avg', 'View')
+    print '%6s %10s %10s %s' % ('Count', 'Time', 'Avg', 'View')
     for view, total_time, count in counts:
         print '%6s %10s %10s %s' % (count, total_time, total_time/float(count), view)
