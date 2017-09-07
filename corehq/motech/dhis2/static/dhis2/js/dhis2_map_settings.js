793bd284f553e79d728c0e544f60ec7691f28eb6
Norman Hooper <nhooper@dimagi.com>
2017-09-07 17:44:58 +0200

    Add option dropdowns to form

--- corehq/motech/dhis2/static/dhis2/js/dhis2_map_settings.js
+++ corehq/motech/dhis2/static/dhis2/js/dhis2_map_settings.js
@@ -8,12 +8,13 @@ hqDefine('dhis2/js/dhis2_map_settings', function () {
     var DataValueMap = function (properties) {
         var self = this;
 
+        self.dataSetMap = properties["dataSetMap"];
         self.ucrColumn = ko.observable(properties["column"]);
+        self.dataElements = ko.observableArray(properties["dataElements"]);
         self.dataElementId = ko.observable(properties["data_element_id"]);
+        self.categoryOptionCombos = ko.observableArray(properties["categoryOptionCombos"]);
         self.categoryOptionComboId = ko.observable(properties["category_option_combo_id"]);
         self.dhis2Comment = ko.observable(properties["comment"]);
-
-        self.errors = [];
 
         self.serialize = function () {
             return {
@@ -32,6 +33,7 @@ hqDefine('dhis2/js/dhis2_map_settings', function () {
         self.ucrId = ko.observable(properties["ucr_id"]);
         self.frequency = ko.observable(properties["frequency"]);
         self.dayOfMonth = ko.observable(properties["day_to_send"]);
+        self.dataSets = ko.observableArray(properties["dataSets"]);
         self.dataSetId = ko.observable(properties["data_set_id"]);
 
         self.orgUnitId = ko.observable(properties["org_unit_id"]);
@@ -48,12 +50,29 @@ hqDefine('dhis2/js/dhis2_map_settings', function () {
         self.completeDate = ko.observable(properties["complete_date"]);
         self.dataValueMaps = ko.observableArray();
 
-        self.errors = [];
+        self.dataElements = ko.observableArray([]);
+        self.categoryOptionCombos = ko.observableArray([]);
 
         self.init = function () {
+            $.get(
+                'datasets/' + self.dataSetId() + '/elems/',
+                {},
+                function (data) {
+                    self.dataElements(data);
+                }
+            );
+            $.get(
+                'datasets/' + self.dataSetId() + '/catopts/',
+                {},
+                function (data) {
+                    self.categoryOptionCombos(data);
+                }
+            );
             if (properties.hasOwnProperty("datavalue_maps") && properties["datavalue_maps"].length > 0) {
                 for (var i = 0; i < properties["datavalue_maps"].length; i++) {
-                    self.dataValueMaps.push(new DataValueMap(properties["datavalue_maps"][i]));
+                    self.dataValueMaps.push(new DataValueMap(
+                        Object.assign(properties["datavalue_maps"][i], {"dataSetMap": self})
+                    ));
                 }
             } else {
                 self.addDataValueMap();
@@ -61,7 +80,7 @@ hqDefine('dhis2/js/dhis2_map_settings', function () {
         };
 
         self.addDataValueMap = function () {
-            self.dataValueMaps.push(new DataValueMap({}));
+            self.dataValueMaps.push(new DataValueMap({"dataSetMap": self}));
         };
 
         self.removeDataValueMap = function (dataValueMap) {
@@ -91,7 +110,7 @@ hqDefine('dhis2/js/dhis2_map_settings', function () {
         };
     };
 
-    module.Dhis2MapSettings = function (dataSetMaps, sendDataUrl) {
+    module.Dhis2MapSettings = function (dataSetMaps, sendDataUrl, dataSets) {
         var self = this;
         var alert_user = hqImport("hqwebapp/js/alert_user").alert_user;
 
@@ -104,7 +123,7 @@ hqDefine('dhis2/js/dhis2_map_settings', function () {
         self.init = function () {
             if (dataSetMaps.length > 0) {
                 for (var i = 0; i < dataSetMaps.length; i++) {
-                    var dataSetMap = new DataSetMap(dataSetMaps[i]);
+                    var dataSetMap = new DataSetMap(Object.assign(dataSetMaps[i], {"dataSets": dataSets}));
                     dataSetMap.init();
                     self.dataSetMaps.push(dataSetMap);
                 }
@@ -114,7 +133,7 @@ hqDefine('dhis2/js/dhis2_map_settings', function () {
         };
 
         self.addDataSetMap = function () {
-            self.dataSetMaps.push(new DataSetMap({}));
+            self.dataSetMaps.push(new DataSetMap({"dataSets": dataSets}));
         };
 
         self.removeDataSetMap = function (dataSetMap) {
