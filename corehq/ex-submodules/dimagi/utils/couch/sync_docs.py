2a7c75858f0352246dd83ee5375f4e28bd5e9f74
Simon Kelly <skelly@dimagi.com>
2017-11-08 13:42:39 +0200

    only index docs if we actually pushed them

--- corehq/ex-submodules/dimagi/utils/couch/sync_docs.py
+++ corehq/ex-submodules/dimagi/utils/couch/sync_docs.py
@@ -21,9 +21,12 @@ def sync_design_docs(db, design_dir, design_name, temp=None):
     """
     design_name_ = '%s-%s' % (design_name, temp) if temp else design_name
     docid = "_design/%s" % design_name_
-    push(design_dir, db, force=True, docid=docid)
-    log.info("synced '%s' in couchdb", design_name)
-    if temp:
+    pushed = push(design_dir, db, force=True, docid=docid)
+    if pushed:
+        log.info("synced '%s' in couchdb", design_name)
+    else:
+        log.info("skipped syncing '%s' to couchdb as it has no changes", design_name)
+    if pushed and temp:
         index_design_docs(db, docid, design_name_)
 
 
